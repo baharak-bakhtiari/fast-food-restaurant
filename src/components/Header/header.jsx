03d0deee1778c506c61b7beffd7493faba6e2b7b
The-little-spring <100764083+The-little-spring@users.noreply.github.com>
2023-06-01 17:55:35 +0330

    make header responsive

--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -1,38 +1,50 @@
-import logo from "../../assets/images/logo.png";
+import { useState } from "react";
 import { BsFillCartFill, BsTelephoneFill } from "react-icons/bs";
-import { RiShoppingCartLine } from "react-icons/ri";
 import { MdRestaurantMenu } from "react-icons/md";
 import { Link } from "react-router-dom";
+import { Navbar, Nav } from "react-bootstrap";
+import logo from "../../assets/images/logo.png";
 import "./header.css";
 import { useAppContext } from "../../context";
 
 const Header = () => {
   const { cart } = useAppContext();
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const handleMenuToggle = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
-    <nav className="navbar navbar-expand-lg">
-      <div className="container d-flex justify-content-between px-0">
-        <div className="container d-flex justify-content-start px-0">
-          <Link className="nav-link" to="/">
-            <MdRestaurantMenu className="nav-icons fs-lg" />
-            منو
-          </Link>
-          <Link className="nav-link navbar-tool" to="/Cart">
-            <span className="navbar-tool-label bg-warning rounded-circle">
-              {cart.length}
-            </span>
-            <BsFillCartFill className="navbar-tool-icon" />
-            سبد خرید
-          </Link>
-          <a className="nav-link" href="tel:410-602-5008">
-            <BsTelephoneFill className="nav-icons fs-lg" />
-            تماس با ما
-          </a>
-        </div>
-        <div>
-          <img src={logo} className="img-fluid logo" alt="logo" />
-        </div>
+    <Navbar
+      expand="lg"
+      className={`navbar ${isMenuOpen ? "menu-open" : ""}`}
+      dir="rtl"
+    >
+      <div className="container position-relative">
+        <Navbar.Toggle aria-controls="navbarNav" onClick={handleMenuToggle} />
+        <Navbar.Collapse id="navbarNav">
+          <Nav className="mr-auto">
+            <Nav.Link id="navitem" as={Link} to="/">
+              <MdRestaurantMenu className="nav-icons fs-lg" />
+              منو
+            </Nav.Link>
+            <Nav.Link id="navitem" as={Link} to="/Cart">
+              <span className="cart-label badge badge-warning rounded-circle">
+                {cart.length}
+              </span>
+              <BsFillCartFill className="navbar-tool-icon" />
+              سبد خرید
+            </Nav.Link>
+            <Nav.Link id="navitem" href="tel:410-602-5008">
+              <BsTelephoneFill className="nav-icons fs-lg" />
+              تماس با ما
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+        <img src={logo} className="img-fluid logo" alt="logo" />
       </div>
-    </nav>
+    </Navbar>
   );
 };
 
